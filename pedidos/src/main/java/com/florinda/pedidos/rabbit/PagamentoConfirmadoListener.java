fd012c74655a6d2cdb602560b6060673df125b80
Jacqueline <jcasalia@yahoo.com.br>
2026-04-30 08:55:35 -0300

    consumindo mensagem no pedido

---
+++ pedidos/src/main/java/com/florinda/pedidos/rabbit/PagamentoConfirmadoListener.java
@@ -0,0 +1,30 @@
+package com.florinda.pedidos.rabbit;
+
+
+import com.florinda.pedidos.config.AmqpConfig;
+import com.florinda.pedidos.domain.enums.StatusPedido;
+import com.florinda.pedidos.domain.model.Pedido;
+import com.florinda.pedidos.repository.PedidoRepository;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class PagamentoConfirmadoListener{
+
+    private final PedidoRepository pedidoRepository;
+
+    public PagamentoConfirmadoListener(PedidoRepository pedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+    }
+
+    @RabbitListener(queues = AmqpConfig.PAGAMENTOS_CONFIRMADO_QUEUE)
+     @Transactional
+    public  void pagamantoConfirmado(PagamentoConfirmadoEvent pagamentoConfirmadoEvent){
+        System.out.println("[Pedidos] Recebido novo pagamento confirmado:" + pagamentoConfirmadoEvent);
+        //atualiza no banco de dados
+        Pedido pedido = pedidoRepository.findById(pagamentoConfirmadoEvent.pedidoId())
+                .orElseThrow();
+        pedido.setStatus(StatusPedido.PAGO);
+    }
+}
